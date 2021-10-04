6b7a7eefe587abd1c1f715c0aaa28470e4aa69a6
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-04 17:34:59 +0200

    handler for default

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,9 +1,19 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
 	//Métodos de acción o Handlers
+	
+	@RequestMapping(value = {"/","/index","home"},method = RequestMethod.GET) //@GetMapping({"/","/index","home"})
+	public String index() {
+		
+		//nombre de la vista
+		return "inex";
+	}
 }
