7c37c9aa0ccab71de883ce0abd01bf0295e8f2c3
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-05 10:45:45 +0200

    example model map

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,7 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap; //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping; //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +11,7 @@ public class IndexController {
 	//Métodos de acción o Handlers
 	
 	@RequestMapping(value = {"/","/index","home"},method = RequestMethod.GET) //@GetMapping({"/","/index","home"})
-	public String index(Model model) {
+	public String index(ModelMap model) {
 		
 		//Agregramos datos a la vista como un mapa de valores {key,value}
 		model.addAttribute("titulo","Hola Spring Framework!");
