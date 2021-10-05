4ad4cf898cd67a502ddf4ec160ad1caf38f25ba6
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-05 11:23:46 +0200

    example with ModelAndView

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 
 //import org.springframework.ui.Model;
 //import org.springframework.ui.ModelMap; 
-import java.util.Map;
+//import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
 
 import org.springframework.web.bind.annotation.RequestMapping; //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class IndexController {
@@ -17,13 +19,16 @@ public class IndexController {
 	//Métodos de acción o Handlers
 	
 	@RequestMapping(value = {"/","/index","home"},method = RequestMethod.GET) //@GetMapping({"/","/index","home"})
-	public String index(Map<String, Object> model) {
+	public ModelAndView index(ModelAndView mv) {
 		
 		//Agregramos datos a la vista como un mapa de valores {key,value}
 		//model.addAttribute("titulo","Hola Spring Framework!");
-		model.put("titulo","Hola Spring Framework!");
-
+		//model.put("titulo","Hola Spring Framework!");
+		mv.addObject("titulo","Hola Spring Framework! con ModelAndView");
+		
 		//nombre de la vista
-		return "index";
+		mv.setViewName("index");
+		//return "index";
+		return mv;
 	}
 }
