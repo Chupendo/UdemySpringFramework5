1f817bfd4923190e46c2fca705610e678c49f1b9
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-05 10:43:34 +0200

    example object model

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping; //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +11,10 @@ public class IndexController {
 	//Métodos de acción o Handlers
 	
 	@RequestMapping(value = {"/","/index","home"},method = RequestMethod.GET) //@GetMapping({"/","/index","home"})
-	public String index() {
+	public String index(Model model) {
 		
+		//Agregramos datos a la vista como un mapa de valores {key,value}
+		model.addAttribute("titulo","Hola Spring Framework!");
 		//nombre de la vista
 		return "index";
 	}
