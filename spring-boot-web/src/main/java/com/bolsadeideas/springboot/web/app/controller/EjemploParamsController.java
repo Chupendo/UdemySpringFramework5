61f410f4a84d4f6dfa413949f84a5f1aa9b7f7ff
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-12 10:18:33 +0200

    captura de varios parametros con la anotacion RequestParam y el objeto HttpRequestParam

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,20 +11,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value="/params")
+@RequestMapping(value = "/params")
 public class EjemploParamsController {
 	private static Logger _log = LoggerFactory.getLogger(EjemploParamsController.class.toString());
-	
+
 	@GetMapping("/index")
 	public String index(Model model) {
 		return "params/index";
 	}
-	
+
 	@GetMapping("/string")
-	public String param(@RequestParam(name="texto",required = false) String texto, Model model) {
-		_log.info("Start handler PARAM");
+	public String param(@RequestParam(name = "texto", required = false) String texto, Model model) {
+		_log.info("Start handler PARAM simple");
+
+		model.addAttribute("resultado", "El texto envadio es: " + texto);
+		return "params/ver";
+	}
+
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		_log.info("Start handler PARAM with multiple params and using the anottation RequestParam");
+
+		model.addAttribute("resultado", "El saludo es '" + saludo + "', y el número es '" + numero + "'");
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		_log.info("Start handler MIX-PARAM with multiple params and using the object HttpServletRequest");
 		
-		model.addAttribute("resultado","El texto envadio es: "+texto);
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		}catch(NumberFormatException e){
+			numero = null;
+		}
+		model.addAttribute("resultado", "El saludo es '" + saludo + "', y el número es '" + numero + "'");
 		return "params/ver";
 	}
 }
