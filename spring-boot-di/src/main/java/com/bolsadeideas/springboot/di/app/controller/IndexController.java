7d185f2bcea07d07622acb52cf6a3a1ee780b744
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-14 16:58:21 +0200

    Ejemplo de acople del servicio al controlador sin inyección de dependencias.

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+
 @Controller
 public class IndexController {
 	
+	//Acople de una clase "service" al contralo
+	private MiServicio servicio = new MiServicio();
+	
 	@GetMapping(value= {"","/","index","home"})
 	public String index(Model model) {
+		
+		//Pasmos a la vista el resutlado de la operaicon del dato que fuimos a buscar
+		model.addAttribute("objeto",servicio.operacion());
 		return "index";
 	}
-
 }
---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+
+public class MiServicio {
+	
+	public String operacion() {
+		return "ejectuando algún proceso imporante...";
+	}
+}
