01c9c3d801375fc4394e198e05924feaa3eecfbd
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-15 21:44:17 +0200

    especificamos al contendoer de ID que el componente "miServicioSimple" es el por defecto

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -1,17 +1,15 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component("miServicioSimple")
-@Primary
+@Component("miServicioSimpleComplejo")
 //@Service
-public class MiServicio implements IServicio{
+public class MiServicioComplejo implements IServicio{
 	//Si hubiera un constructor con parámetros se debe crear uno por defecto (Sin parámetros) de lo contrario daría error al crear la instancia por parte del contenedor de dependencias de spring.
 	
 	@Override //Esta anotación indica que es una operación del padre
 	public String operacion() {
-		return "ejectuando algún proceso imporante simple...";
+		return "ejectuando algún proceso imporante complejo...";
 	}
 }
