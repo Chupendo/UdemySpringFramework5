b0267edd5ea4f601672c57299a497fc8f1e65ef0
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-15 21:18:53 +0200

    dejamos solo inyeccion de dependencia del servicio via atributo

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -12,34 +12,10 @@ import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 @Controller
 public class IndexController {
 	
-	//Acople de una clase "service" al contralor
-	//private MiServicio servicio = new MiServicio();
-	
-	//Inyección de dependencias(ID) DTO
-	//@Autowired
-	//private MiServicio servicio;
-	
 	//Inyección de dependencias por interfaz
-	//@Autowired //por defecto coge la primea que encuentre anotada con componete e implemente la intefaz
-	//@Qualifier("miServicioSimple") 
-	//private IServicio servicio;
-	
-	//Inyección de dependencias de la interfaz de servicio por método setter
-	/*
+	@Autowired //por defecto coge la primea que encuentre anotada con componete e implemente la intefaz
 	private IServicio servicio;
 	
-	@Autowired
-	public void setServicio(IServicio servicio) {
-		this.servicio = servicio;
-	}
-	*/
-	//Inyección de dependencias de la interfaz de servicio por constructor
-	private IServicio servicio;
-	
-	@Autowired //Anotación opcional, vara ID del servicio primario vía constructor 
-	public IndexController(IServicio servicio) {
-		this.servicio=servicio;
-	}
 		
 	@GetMapping(value= {"","/","index","home"})
 	public String index(Model model) {
