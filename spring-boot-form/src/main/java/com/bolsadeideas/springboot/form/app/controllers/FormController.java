ba6fbf577e4e4d123f136e6d173733a7e6cffc1e
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-23 17:29:34 +0200

    creating controller and your methods handler for getting and processing a form

---
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.form.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+	//Procesa la peticion de tipo post que va a form, para devolver el formualrio con los datos a procesar por otro handler.
+	@GetMapping("/form")
+	public String obtenerFormulario() {
+		return "form";
+	}
+	
+
+	//Procesa la peticion de tipo post que va a form, para obtener los datos.
+	@PostMapping("/form")
+	public String leerFormuario() {
+		return "resultado";
+	}
+}
