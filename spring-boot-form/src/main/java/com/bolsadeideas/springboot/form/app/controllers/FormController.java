6995f4ab18635c7336e7b3b50051474454d2d68e
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-23 18:01:06 +0200

    adding the dependency spring-boot-starter-validation

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,14 +10,14 @@ public class FormController {
 
 	//Procesa la peticion de tipo post que va a form, para devolver el formualrio con los datos a procesar por otro handler.
 	@GetMapping("/form")
-	public String obtenerFormulario() {
+	public String obtenerFormulario(Model model) {
 		return "form";
 	}
 	
 
 	//Procesa la peticion de tipo post que va a form, para obtener los datos.
 	@PostMapping("/form")
-	public String leerFormuario() {
+	public String leerFormuario(Model model) {
 		return "resultado";
 	}
 }
