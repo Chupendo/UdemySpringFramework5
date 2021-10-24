51cd5f3cb04445395bb20205069c716707c62366
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-10-24 08:51:29 +0200

    ejemplo que muestra com procesar los datos de un formulario tipo post con ParamRequest y devuelve los resultados a una vista html con thymeleaf

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -11,13 +12,23 @@ public class FormController {
 	//Procesa la peticion de tipo post que va a form, para devolver el formualrio con los datos a procesar por otro handler.
 	@GetMapping("/form")
 	public String obtenerFormulario(Model model) {
+		model.addAttribute("titulo", "Formulario usuarios");
 		return "form";
 	}
 	
 
 	//Procesa la peticion de tipo post que va a form, para obtener los datos.
 	@PostMapping("/form")
-	public String leerFormuario(Model model) {
+	public String leerFormuario(Model model,
+			@RequestParam(name = "username") String username,
+			@RequestParam(value = "password") String pwd,
+			@RequestParam String email) {
+		
+		model.addAttribute("titulo", "Resutlado form");
+		model.addAttribute("username", username);
+		model.addAttribute("password", pwd);
+		model.addAttribute("email", email);
+		
 		return "resultado";
 	}
 }
