69f837e3c936abb7712ca19eaf7590685c77355d
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-11-01 17:45:34 +0100

    ejemplo de validacion del atributo Usuario.nombre no este vacio a través de la clase validarora personalizada

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
@@ -1,20 +1,51 @@
 package com.bolsadeideas.springboot.form.app.validation;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import com.bolsadeideas.springboot.form.app.model.domain.Usuario;
+
+@Component
 public class UsuarioValidador implements Validator {
 
 	@Override
+	/*
+	 * Determina el tipo de clase a validr
+	 * 
+	 * @param clazz:Class<?> Clase validar
+	 * @return true si clazz es de tipo Usuario, false resto de casos
+	 */
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
-		return false;
+		//Para validar que el objeto estamos validadno o que vamos a validar corresponde a la clase de tipo Usuario y no otro,
+		//se ha de comprobar si la clase Usuario es asignable desde la clase que recibe el metodo como parámetro de entrada
+		return Usuario.class.isAssignableFrom(clazz);
 	}
 
 	@Override
+	/*
+	 * Valida el objeto
+	 * 
+	 * @param targer:Objet Objeto con la informacion a validar
+	 * @param error:Errors Objeto con la informacion de error, registrada por "Reject Error" de org.springframework.validation.ValidationUtils.
+	 */
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		//El param target en este caso es de tipo Usuario por lo que hacemos un cast
+		Usuario usuario =(Usuario) target; //Casting
 
+		//validar que el atributo "nombre" no sea vacío >> @NotEmapty
+			//ValidationUtils.rejectIfEmpty(Errors, Fields, ErrorCode);
+			//Errors: objeto que contiene el mensaje de error
+			//Fields: mismo nombre del atributo en la clase pojo a validar
+			//errorCode: Key del mensaje error registrado en el "messages.properties"
+		ValidationUtils.rejectIfEmpty(errors, "nombre", "NotEmpty.user.nombre");
+		
+		//RejectIfEmpty es lo mismo que usar la estrucutra de control if con isEmpaty
+		/*
+		if(usuario.getNombre().isEmpty()) {
+			errors.rejectValue("nombre", "NotEmpty.user.nombre");
+		}
+		*/
 	}
-
 }
