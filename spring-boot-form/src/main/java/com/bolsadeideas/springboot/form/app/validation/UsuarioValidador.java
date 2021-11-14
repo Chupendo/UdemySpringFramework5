9dc471c4cc65ed655abce836db05905d1f1a005a
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-11-14 19:00:16 +0100

    mensaje personalizado "requerido.user.nombre" cuando la clase validaroa detecta espacioes en blanco o el campo nombre vacío:
    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre",
    "requerido.user.nombre");

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
@@ -41,6 +41,7 @@ public class UsuarioValidador implements Validator {
 			//errorCode: Key del mensaje error registrado en el "messages.properties"
 		//ValidationUtils.rejectIfEmpty(errors, "nombre", "NotEmpty.user.nombre"); //Validar vacío
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre", "NotEmpty.user.nombre"); //Validar vacío y espacios en blanco
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre", "requerido.user.nombre"); //Validar vacío y espacios en blanco con mensaje personalizado
 		//RejectIfEmpty es lo mismo que usar la estrucutra de control if con isEmpaty
 		/*
 		if(usuario.getNombre().isEmpty()) {
