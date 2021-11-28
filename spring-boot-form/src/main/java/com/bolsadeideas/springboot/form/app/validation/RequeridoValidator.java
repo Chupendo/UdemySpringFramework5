1e95c5a83aa9fe5f966b3263ccfb4b776c0434dc
andres.rpenuela <andres.rpenuela@2adrerui5511X.babel-si.local>
2021-11-28 17:08:24 +0100

    Se simplica “value.isEmpty() || value.isBlank()“ por “!StringUtils.hasText(value)” de la clase de “spring.framerwork”.

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/RequeridoValidator.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/validation/RequeridoValidator.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.form.app.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.util.StringUtils;
+
 public class RequeridoValidator implements ConstraintValidator<Requerido, String> {
 	private Requerido requerido;
 
@@ -17,7 +19,13 @@ public class RequeridoValidator implements ConstraintValidator<Requerido, String
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 
+		/*
 		return (value == null || value.isEmpty() || value.isBlank() || value.length() < this.requerido.length())
+				? false
+				: true;
+		*/
+		
+		return (value == null || !StringUtils.hasText(value)|| value.length() < this.requerido.length())
 				? false
 				: true;
 
