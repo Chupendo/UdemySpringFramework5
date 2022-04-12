57e180aa9e3a1bbc0f83bdc6fdc37727bea4e5ab
Chupendo <andresruizpenuela@gmail.com>
2022-04-12 11:03:16 +0200

    esquelo del proyecto web para el uso de interceptores

--- spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/controller/AppController.java
+++ spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/controller/AppController.java
@@ -1,5 +1,19 @@
 package com.bolsadeideas.springboot.horariointerceptor.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/control")
 public class AppController {
 
+	// Metodo handler
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Bienvenido al horario de atención a clientes");
+
+		return "index";
+	}
 }
