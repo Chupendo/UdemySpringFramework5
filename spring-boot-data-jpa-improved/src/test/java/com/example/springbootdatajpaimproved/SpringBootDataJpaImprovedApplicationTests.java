e5fbccb4423d32c144fa89804e67e8e55412880f
Chupendo <andresruizpenuela@gmail.com>
2022-05-08 20:39:54 +0200

    init branch project empaty

---
+++ spring-boot-data-jpa-improved/src/test/java/com/example/springbootdatajpaimproved/SpringBootDataJpaImprovedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatajpaimproved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaImprovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
